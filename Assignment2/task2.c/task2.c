ee2a4542ecdcc84c07abe3ab18fe4a252150a1c2
Michael Kimani <kimanimichael819@gmail.com>
2025-11-16 01:31:17 +0300

    Add Assignment2 files

--- Assignment2/task2.c/task2.c
+++ Assignment2/task2.c/task2.c
@@ -2,10 +2,10 @@
 #define pi 3.14
 int main(void){
     float radius , surface_area;
-    printf("Enter the radius of the circle: ");
+    printf("Enter the radius of the sphere: ");
     scanf("%f", &radius);
     surface_area = 4 * pi * radius * radius;
-    printf("The surface area of the circle is: %.2f\n", surface_area);
+    printf("The surface area of the sphere is: %.2f\n", surface_area);
     return 0;
 }
 
